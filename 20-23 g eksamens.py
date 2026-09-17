adc4e6b7e1ff496436c69b3e14143d7de0d54a04
markussbprogrammas <markussberzins2008@gmail.com>
2026-09-17 09:52:28 +0300

    Update 20-23 g eksamens.py

--- 20-23 g eksamens.py
+++ 20-23 g eksamens.py
@@ -1,1 +1,19 @@
 #python
+from datetime import datetime
+class masinas:
+    def __init__ ( self, zimols, modelis, regd, masa, benzins ):
+        self.laiks = datetime.now().strftime("%Y-%M-%d")
+        self.zimols = zimols
+        self.modelis = modelis
+        self.regd = regd
+        self.masa = masa
+        self.benzins = benzins
+
+auto_tests = masinas
+zimols = "audi"
+modelis = "A4"
+regd = "22.10.2019"
+masa = 1800
+degviela = "BG"
+
+print(zimols, modelis, regd, masa, degviela)
